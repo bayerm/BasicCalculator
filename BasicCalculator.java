700f6f18de47cf9d130f629f6b58f60b807420a6
Morgan <mbayer.school@gmail.com>
2015-07-07 16:17:58 -0700

    Added Comments

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 class BasicCalculator{
 	public static void main(String args[]){
-		Scanner scan = new Scanner(System.in)l
+		Scanner scan = new Scanner(System.in);
 		double fnum, snum, answer;
-		System.out.println("Enter first num: ");
+		System.out.println("Enter first num: "); //asks user for first number
 		fnum = scan.nextDouble();
-		System.out.println("Enter second num: ");
+		System.out.println("Enter second num: "); //asks user for second number
 		snum = scan.nextDouble();
 		answer = fnum + snum;
 		System.out.println(answer);
